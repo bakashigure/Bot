95a3c31fb0e111bcf0ee4e31b907a7ec84d847ee
Tastror <yccozy2001@163.com>
2024-03-13 01:42:00 +0800

    update chatgpt

--- src/plugins/chatgpt/__init__.py
+++ src/plugins/chatgpt/__init__.py
@@ -1,10 +1,15 @@
 import re
+import yaml
+from openai import OpenAI
 from nonebot.log import logger
 from nonebot.rule import startswith
 from nonebot import on_message, on_regex
 from nonebot.adapters.onebot.v11 import Bot, Event
 
 gpt_catcher = on_regex(r"^gpt\r?\n((?:.|\s)*?)$")
+
+with open("./src/plugins/chatgpt/gpt.yaml", 'r', encoding='utf-8') as file:
+    yaml_read = yaml.safe_load(file.read())
 
 @gpt_catcher.handle()
 async def _(bot: Bot, event: Event):
@@ -15,6 +20,19 @@ async def _(bot: Bot, event: Event):
     raw_msg = event_dict['raw_message']
 
     content = re.findall(r"^gpt\r?\n((?:.|\s)*?)$", raw_msg)[0]
-    reply = f'哈哈这是{content}'
-    await gpt_catcher.send(reply)
+
+    client = OpenAI(
+        api_key=yaml_read['api_key'],
+        base_url=yaml_read['base_url']
+    )
+
+    completion = client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": content}
+        ]
+    )
+
+    await gpt_catcher.send(completion.choices[0].message)
     await gpt_catcher.finish()
