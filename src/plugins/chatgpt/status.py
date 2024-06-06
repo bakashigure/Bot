c746275025f35ac86abdbc8f82d75388bea7906e
Tastror <yccozy2001@163.com>
2024-06-07 02:22:21 +0800

    debug

--- src/plugins/chatgpt/status.py
+++ src/plugins/chatgpt/status.py
@@ -4,7 +4,7 @@ from collections import Counter
 def get_status(date, group):
     print(date, group)
     log = rf"""{str(date)}-[0-9]*? .*? - __init__ - INFO - send message to
-\[message.group.normal\]: Message -[0-9]*? from ([0-9]*?)@\[群:{str(group)}\]"""
+\[message.group.normal\]: Message -?[0-9]*? from ([0-9]*?)@\[群:{str(group)}\]"""
     print(log)
 
     with open("../gpt.log") as f:
